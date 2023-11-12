c73e3a37626ceb5832b97be6b6e8a836a1eae418
Aquib Iqbal <aquib.iq.1011@gmail.com>
2023-11-12 01:15:29 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# hackumass
+# HackUMass-XI 2023
+## MoodMeter.AI
+### Overview
+MoodMeter.AI is designed to enhance online communication in remote work environments by providing real-time emotional intelligence. Utilizing facial expression and voice analysis, our tool offers insights into the emotional state of meeting participants, fostering better understanding and collaboration in online meetings.
+
+### Features
+* Real-Time Emotion Detection: Leverages machine learning to analyze facial expressions and voice tones, offering instant feedback on the emotional state of participants.
+* Facial Expression Analysis: Uses advanced facial recognition technology to interpret non-verbal cues, essential for understanding nuanced communication.
+* Voice Sentiment Analysis: Processes vocal inputs to determine sentiment, aiding in the perception of tone and intent.
+* Interactive Dashboard: Provides a user-friendly interface with real-time analytics and summaries of emotional cues.
+### Tech Stack
+* React.js: Ensures a dynamic and responsive user interface, catering to interactive user experiences.
+* Node.js and Express.js: Creates a robust server-side architecture that is scalable and efficient.
+* Python: Powers our advanced machine learning models for accurate and fast sentiment analysis.
+* Agora SDK: Enables real-time video communication, essential for analyzing online meeting interactions.
+* LLM (Language Learning Models): Utilized for sophisticated text summarization and sentiment analysis, enhancing the accuracy of our emotion detection algorithms.
+### Installation
+
+* Install Dependencies: Navigate to the project directory and run npm install to install all required dependencies.
+ ```
+ $ cd client && npm install
+```
+* Environment Setup: Configure the .env file with necessary API keys and settings.
+```
+Agora_API = "INSERT YOUR API KEY"
+```
+### How to Run
+* Launch the Application: In a separate terminal, execute npm start to launch the React application.
+```
+$ npm run-script start
+```
+* Access the Application: Open a web browser and visit http://localhost:3000 to access MoodMeter.AI.
+Usage
+* Join a Meeting: Log into the platform and join an online meeting.
+* Enable Emotion Analysis: Activate the emotion detection feature to start analyzing the emotional state of participants.
+* View Insights: Real-time emotional insights are displayed on the dashboard, offering an enhanced understanding of the meeting dynamics.
+
+### Contributing
+We welcome contributions to MoodMeter.AI! If you're interested in improving our application or adding new features, please follow these steps:
+
+Fork the Repository: Create a personal fork of the project on GitHub.
+Create a New Branch: For each new feature or improvement, create a separate branch.
+Submit a Pull Request: After making your changes, submit a pull request for review.
--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -8,7 +8,7 @@ const About = () => {
             <section className="about__description">
                 <p>
                     Moodmeter.ai is an innovative platform designed to revolutionize emotional analysis in video communications. 
-                    Utilizing cutting-edge machine learning algorithms, Moodmeter.ai captures and interprets the sentiment of participants 
+                    Utilizing cutting-edge machine learning algorithms, it captures and interprets the sentiment of participants 
                     during video calls, offering a comprehensive understanding of the emotional tone of the conversation.
                 </p>
                 <p>
@@ -20,13 +20,26 @@ const About = () => {
             <section className="about__tech-stack">
                 <h2>Our Technology Stack</h2>
                 <p>
-                    Moodmeter.ai is built with a combination of modern and robust technologies, ensuring reliability, efficiency, and scalability:
+                    Built with a combination of modern and robust technologies, Moodmeter.ai ensures reliability, efficiency, and scalability:
                 </p>
                 <ul>
                     <li>React.js for a responsive and interactive user interface</li>
-                    <li>Node.js and Express.js for a powerful and efficient server-side architecture</li>
-                    <li>Advanced machine learning models, primarily in Python, for accurate sentiment analysis</li>
+                    <li>Node.js and Express.js for a powerful server-side architecture</li>
+                    <li>Advanced machine learning models in Python for accurate sentiment analysis</li>
                     <li>Agora SDK for seamless real-time video communication</li>
+                </ul>
+            </section>
+            <section className="about__business-value">
+                <h2>Business Value and Impact</h2>
+                <p>
+                    Moodmeter.ai delivers significant value to businesses by enhancing communication effectiveness and emotional engagement.
+                    In today's digital-first world, understanding emotional cues is crucial for successful interactions, especially in remote
+                    and virtual environments.
+                </p>
+                <ul>
+                    <li>Improves team dynamics and productivity through better understanding of emotional feedback</li>
+                    <li>Enables businesses to gauge customer reactions in real-time, leading to improved service and satisfaction</li>
+                    <li>Offers valuable insights for training and development in HR settings</li>
                 </ul>
             </section>
             <section className="about__applications">
@@ -40,6 +53,14 @@ const About = () => {
                     <li>Enriching online learning experiences by assessing student engagement</li>
                     <li>Improving customer interactions and satisfaction in service industries</li>
                 </ul>
+            </section>
+            <section className="about__future-vision">
+                <h2>Our Future Vision</h2>
+                <p>
+                    At Moodmeter.ai, we are committed to continuous innovation and expansion. Our future roadmap includes integrating more
+                    sophisticated AI algorithms for nuanced emotion recognition, expanding our market reach to include new sectors, and
+                    enhancing user experience with intuitive features.
+                </p>
             </section>
         </div>
     );
